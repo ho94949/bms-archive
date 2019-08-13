5ae1d94086c77cd24581a7a74622ae333f8e4b23
ho94949 <sakura@kyouko.moe>
2019-08-14 00:11:14 +0900

    Modified GuessKey function. (Should be changed more aggresively), PEP8 check

--- server-script/loadbms.py
+++ server-script/loadbms.py
@@ -12,66 +12,68 @@ from config import *
 from bmsparser import BMS
 
 
-
 def GuessKey(keyList):
 
     def RemoveImplicitSubtitle(title):
-        if title == '': return title
+        if title == '':
+            return title
         sublist = ['--', '～～', '()', '[]', '<>', '""']
-        
+
         for s, e in sublist:
             if title[-1] == e:
                 ind = title.rfind(s)
                 if ind == -1:
-                    return title
+                    return title.strip()
                 else:
-                    return title[:ind]
-        
-        return title
-    
+                    return title[:ind].strip()
+
+        return title.strip()
+
     keyList = list(map(RemoveImplicitSubtitle, keyList))
-    
-    return os.path.commonprefix(keyList)
-    
-    
+
+    if(all(x == keyList[0] for x in keyList)):
+        return keyList[0]
+
+    return "N/A_" + keyList[0]
+
+
 def ExtractZIPData(zipName):
     zipName = ChangeZIPName(zipName)
     zipCode, ext = os.path.splitext(os.path.basename(zipName))
-    
+
     zipJSON = {}
     zipJSON['code'] = zipCode
     zipJSON['bms'] = []
-    
+
     extractDir = ExtractZIP(zipName, EXTRACT_FOLDER_NAME)
-    
+
     for fileName in os.listdir(extractDir):
         fileName = os.path.join(extractDir, fileName)
         if IsBMSFile(fileName):
             bms = BMS(fileName)
-            
+
             bmsJSON = {}
-            
+
             bmsJSON['title'] = bms.getTitle()
             bmsJSON['md5'] = bms.getMD5()
             bmsJSON['artist'] = bms.getArtist()
-            
+
             zipJSON['bms'].append(bmsJSON)
-    
+
     for key in ['title', 'artist']:
         keyList = list(map(lambda s: s[key], zipJSON['bms']))
         zipJSON[key] = GuessKey(keyList)
-        if zipJSON[key] == '':
-            zipJSON[key] = 'N/A_' + zipCode
-    
-    jsonFilename = os.path.join(JSON_FOLDER_NAME, zipCode + '.json') 
+
+    jsonFilename = os.path.join(JSON_FOLDER_NAME, zipCode + '.json')
 
     with open(jsonFilename, 'w') as jsonFile:
-        jsonFile.write(json.dumps(zipJSON, sort_keys=True,indent=4))
+        jsonFile.write(json.dumps(zipJSON, sort_keys=True, indent=4))
         jsonFile.write('\n')
-    
+
     os.rename(zipName, os.path.join(ZIP_FOLDER_NAME, zipCode + '.zip'))
-    
+
     print('OK! [{0}] {1}'.format(zipJSON['artist'], zipJSON['title']))
+
 
 def main():
     """Main Logic"""
