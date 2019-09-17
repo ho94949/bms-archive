6c160c5e9607c6de1166f6475cef8324c1c801af
ho94949 <sakura@kyouko.moe>
2019-09-17 13:35:25 +0900

    Re-minify

--- server-script/bmspackage.py
+++ server-script/bmspackage.py
@@ -13,10 +13,9 @@ def MakePackage(zipName, normalizedName, minifiedName, infoFile):
     basename, _ = os.path.splitext(os.path.basename(zipName))
 
     originalDir = os.path.join(EXTRACT_FOLDER_NAME, basename)
-    normalizeDir = os.path.join(NORMALIZE_EXTRACT_FOLDER_NAME, basename)
     minifyDir = os.path.join(MINIFY_EXTRACT_FOLDER_NAME, basename)
 
-    for d in [originalDir, normalizeDir, minifyDir]:
+    for d in [originalDir, minifyDir]:
         if not os.path.isdir(d):
             os.mkdir(d)
 
@@ -35,6 +34,8 @@ def MakePackage(zipName, normalizedName, minifiedName, infoFile):
 
     assert len(BMSList) > 0, "Empty BMS file not allowed"
 
+    origDirContent = bmsutil.ListFolder(extractDir)
+
     WAVList = set()
     BMPList = set()
     StageList = set()
@@ -42,8 +43,6 @@ def MakePackage(zipName, normalizedName, minifiedName, infoFile):
     for e, b in BMSList:
 
         fname = b.md5 + '.' + e
-        with open(os.path.join(normalizeDir, fname), 'wb') as f:
-            f.write(b.raw)
 
         with open(os.path.join(minifyDir, fname), 'wb') as f:
             f.write(b.raw)
@@ -66,41 +65,14 @@ def MakePackage(zipName, normalizedName, minifiedName, infoFile):
             if os.path.exists(q) and not os.path.isdir(q):
                 namef, _ = os.path.splitext(f)
                 bmsutil.NormalizeWav(q, os.path.join(
-                    normalizeDir, namef + '.ogg'))
-                shutil.copy(os.path.join(normalizeDir, namef+'.ogg'),
-                            os.path.join(minifyDir, namef+'.ogg'))
+                    minifyDir, namef + '.ogg'))
                 break
         else:
             ErrorList.append('WAV File not found: ' + f)
 
-    for f in BMPList | StageList:
-        if f == '':
-            continue
-        ff = os.path.join(extractDir, f)
-
-        name, ext = os.path.splitext(ff)
-
-        fileSearchOrder = [ff, name + '.bmp', name + '.png', name+'.jpg']
-
-        for q in fileSearchOrder:
-            if os.path.exists(q) and not os.path.isdir(q):
-                namef, _ = os.path.splitext(f)
-                _, ext = os.path.splitext(q)
-
-                if ext == '.bmp':
-                    bmsutil.NormalizeBmp(q, os.path.join(
-                        normalizeDir, namef+'.png'))
-                else:
-                    shutil.copy(q, os.path.join(normalizeDir, namef+ext))
-                break
-        else:
-            ErrorList.append('BMP file not found: ' + f)
-
-    bmsutil.CompressFolder(normalizeDir, normalizedName)
     bmsutil.CompressFolder(minifyDir, minifiedName)
 
     shutil.rmtree(extractDir)
-    shutil.rmtree(normalizeDir)
     shutil.rmtree(minifyDir)
 
     bmsinfo = {'bms': []}
@@ -148,6 +120,8 @@ def MakePackage(zipName, normalizedName, minifiedName, infoFile):
 
     bmsinfo['title'] = Title
     bmsinfo['artist'] = Artist
+    bmsinfo['error'] = ErrorList
+    bmsinfo['content'] = origDirContent
 
     with open(infoFile, 'w') as f:
         f.write(json.dumps(bmsinfo, ensure_ascii=False, indent=2, sort_keys=True))
--- server-script/bmsutil.py
+++ server-script/bmsutil.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 from PIL import Image
 
+
 def MD5(filename):
     """Calculate MD5 value of given file."""
     hash_md5 = hashlib.md5()
@@ -60,19 +61,17 @@ def NormalizeWav(filename, destfilename):
 
     destdir = os.path.abspath(os.path.dirname(destfilename))
 
-    if ext.lower() == '.ogg':
-        shutil.copyfile(filename, destfilename)
-    else:
-        shutil.copyfile(filename, os.path.join(
-            destdir, '____bms_archive_convert____.wav'))
+    shutil.copyfile(filename, os.path.join(
+        destdir, '____bms_archive_convert____' + ext))
 
-        subprocess.check_call(['ffmpeg', '-i', '____bms_archive_convert____.wav', '-acodec', 'flac', '____bms_archive_convert____.ogg', '-y'], cwd=destdir,
-                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+    subprocess.check_call(['ffmpeg', '-i', '____bms_archive_convert____' + ext, '-aq', '4', '____bms_archive_convert____result____.ogg', '-acodec', 'libvorbis', '-y'], cwd=destdir,
+                          stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
 
-        os.rename(os.path.join(
-            destdir, '____bms_archive_convert____.ogg'), destfilename)
+    os.rename(os.path.join(
+        destdir, '____bms_archive_convert____result____.ogg'), destfilename)
 
-        os.remove(os.path.join(destdir, '____bms_archive_convert____.wav'))
+    os.remove(os.path.join(destdir, '____bms_archive_convert____' + ext))
+
 
 def NormalizeBmp(filename, destfilename):
     x = Image.open(filename)
@@ -89,12 +88,14 @@ def CompressFolder(src, dst):
             zf.write(absname, arcname)
     zf.close()
 
+
 def ListFolder(src):
     ret = {'isdir': True, 'content': {}}
     abs_src = os.path.normpath(os.path.abspath(src))
     for dirname, subdirs, files in os.walk(src):
         for filename in files:
-            absname = os.path.normpath(os.path.abspath(os.path.join(dirname, filename)))
+            absname = os.path.normpath(
+                os.path.abspath(os.path.join(dirname, filename)))
             orgdir = absname[len(abs_src)+1:].split(os.sep)
 
             md5v = MD5(absname)
@@ -103,7 +104,8 @@ def ListFolder(src):
             filetype = subprocess.check_output(['file', '-b', absname])
             filetype = filetype.decode('utf-8')
             for i in orgdir[:-1]:
-                if i not in ptr['content']: ptr['content'][i] = {'isdir': True, 'content': {}}
+                if i not in ptr['content']:
+                    ptr['content'][i] = {'isdir': True, 'content': {}}
                 ptr = ptr['content'][i]
 
             ptr['content'][orgdir[-1]] = {
